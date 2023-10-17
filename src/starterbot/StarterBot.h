ac5a8decba6069ce1d885790af4ddc3c1ffedde0
estebanSulcaInfante <esteban.sulca@utec.edu.pe>
2023-10-17 07:02:14 -0500

    Add Files via command line

---
+++ src/starterbot/StarterBot.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "MapTools.h"
+
+#include <BWAPI.h>
+
+class StarterBot
+{
+    MapTools m_mapTools;
+	int buildOrderStep;
+    // helper functions to get you started with bot programming and learn the API
+    void sendIdleWorkersToMinerals();
+    void trainAdditionalWorkers();
+    void buildAdditionalSupply();
+    void drawDebugInformation();
+
+
+public:
+
+    StarterBot();
+
+    // functions that are triggered by various BWAPI events from main.cpp
+	void makeTheWorkersCircle();
+
+	void onStart();
+	void onFrame();
+	void onEnd(bool isWinner);
+	void onUnitDestroy(BWAPI::Unit unit);
+	void onUnitMorph(BWAPI::Unit unit);
+	void onSendText(std::string text);
+	void onUnitCreate(BWAPI::Unit unit);
+	void onUnitComplete(BWAPI::Unit unit);
+	void onUnitShow(BWAPI::Unit unit);
+	void onUnitHide(BWAPI::Unit unit);
+	void onUnitRenegade(BWAPI::Unit unit);
+
+};
